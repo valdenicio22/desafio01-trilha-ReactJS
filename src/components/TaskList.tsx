9cac04aaca8f39a27d6d9ce0cac3db79f91a0c0c
valdenicio22 <valdenicio22@gmail.com>
2021-03-15 08:58:59 -0300

    :tada: Challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+  
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, newTask]);
+    
   }
-
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const checkChange = tasks.map(obj => obj.id === id ? 
+    {
+      ...obj,
+      isComplete: !obj.isComplete
+    }: obj )
+    setTasks(checkChange);
+
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksFilter = tasks.filter(obj => obj.id !== id)
+    setTasks(tasksFilter);
   }
 
   return (
